513c8063555758aaf5288a8dc7921e885125aac6
akalyaliya <akalyatg226@gmail.com>
2023-02-02 05:47:54 +0000

    first commit

---
+++ operators.py
@@ -0,0 +1,11 @@
+a=10
+print(type(a))
+a=10.89
+print(type(a))
+a=10+3j
+print(type(a))
+no=10
+print("the decimal value of",no,"is:")
+print(bin(no),"in binary.")
+print(oct(no),"in  octal.")
+print(hex(no),"in hexadecimal.")
